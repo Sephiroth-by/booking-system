fcdac01c4778cdc95142cfee8ce46274fa8061ee
Oleg Ilenkov <sephiroth.by@gmail.com>
2019-06-30 15:54:33 +0300

    get sessions by movie id

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -2,12 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 const authRoutes = require('./auth');
-// const sessionRoutes = require('./session');
+const sessionRoutes = require('./session');
 const movieRoutes = require('./movie');
 // const orderRoutes = require('./order');
 
 router.use('/auth', authRoutes);
-// router.use('/session', sessionRoutes);
+router.use('/session', sessionRoutes);
 router.use('/movie', movieRoutes);
 // router.use('/order', orderRoutes);
 
---
+++ server/src/routes/session.js
@@ -0,0 +1,44 @@
+const router = require('express').Router();
+const authMiddleware = require('../helpers/authMiddleware');
+const Movie = require('../models/Movie');
+const Session = require('../models/Session');
+
+router.get('/:movieId', authMiddleware, async (req, res) => {
+  const movieId = req.params.movieId;
+
+  if (!movieId) {
+    return res.status(400).json({
+      error: 'Movie id missing'
+    });
+  }
+
+  const movie = await Movie.findOne({
+    _id: movieId
+  });
+
+  const sessions = await Session.find({
+    $and: [
+      {
+        movieId: movie._id
+      },
+      {
+        schedule: {
+          $elemMatch: {
+            startTime: {
+              $gt: new Date().toISOString()
+            }
+          }
+        }
+      }
+    ]
+  });
+
+  const data = {
+    movie: movie,
+    sessions: sessions
+  }
+
+  res.json(data);
+});
+
+module.exports = router;
