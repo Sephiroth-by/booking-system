01d20e653e0384ecd902592302f2690ed75837cf
Oleg Ilenkov <oleg.ilenkov@itechart-group.com>
2019-07-30 17:19:05 +0300

    header styling

--- client/src/components/FooterComponent.js
+++ client/src/components/FooterComponent.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { withStyles } from '@material-ui/core';
+
+const styles = {
+  footer: {
+    textAlign: 'left',
+    padding: '20px',
+    marginTop: '20px',
+  },
+};
 
 function Footer(props) {
+  const classes = props.classes;
+
   return (
-    <div>Footer</div>
+    <div className={classes.footer}>© 2019 Movies</div>
   );
 }
 
-export default Footer;
+export default withStyles(styles)(Footer);
--- client/src/components/HeaderComponent.js
+++ client/src/components/HeaderComponent.js
@@ -3,6 +3,8 @@ import {Link} from 'react-router-dom';
 import { connect } from 'react-redux';
 import {authLogOut, checkToken} from '../redux/actionCreators';
 
+import { Button, Typography, Toolbar, AppBar, withStyles } from '@material-ui/core';
+
 const mapStateToProps = state => ({
   user: state.user,
 });
@@ -12,35 +14,56 @@ const mapDispatchToProps = (dispatch) => ({
   checkToken: () => { dispatch(checkToken()); },
 });
 
+const styles = {
+  linkWrapper: {
+    flexGrow: 1,
+    textAlign: 'right',
+  },
+  link: {
+    color: '#fff',
+    textDecoration: 'none',
+    padding: '10px',
+  },
+};
+
 class Header extends Component {
+
   componentDidMount() {
     this.props.checkToken();
   }
 
   render() {
+    const classes = this.props.classes;
+
     let links;
     if(this.props.user.loggedIn) {
-      links = <div>
-        <Link to='/orders'>My Orders</Link>
-        <button type='button' onClick={this.props.authLogOut}>Log Out</button>
+      links = <div className={classes.linkWrapper}>
+        <Link className={classes.link} to='/orders'>My Orders</Link>
+        <Button className={classes.link} onClick={this.props.authLogOut}>Log Out</Button>
       </div>;
+
     }
     else {
-      links = <div>
-        <Link to='/login'>Login</Link>
-        <Link to='/register'>Register</Link>
+      links = <div className={classes.linkWrapper}>
+        <Link className={classes.link} to='/login'>Login</Link>
+        <Link className={classes.link} to='/register'>Register</Link>
       </div>;
     }
 
     return (
-      <div>
-        <div>Header</div>
-        <Link to='/'>Home</Link>
-        {links}
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" className={classes.title}>
+              <Link className={classes.link} to='/'>Movies</Link>
+            </Typography>
+            {links}
+          </Toolbar>
+        </AppBar>
       </div>
     );
   }
 
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Header);
+export default connect(mapStateToProps, mapDispatchToProps)(withStyles(styles)(Header));
